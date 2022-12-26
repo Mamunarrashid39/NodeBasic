5ac5f23a496ccbf824313bba669e98838085f249
Mamunarrashid39 <mamunarrashid.cse@gmail.com>
2022-12-26 22:56:08 +0600

    npm init

---
+++ Lesson5/index.js
@@ -0,0 +1,8 @@
+// const randomFruitsName = require('random-fruits-name');
+// console.log(randomFruitsName( ));
+
+// const { Console } = require('console');
+
+var moviesNames = require('movies-names');
+
+console.log(moviesNames.random() );
