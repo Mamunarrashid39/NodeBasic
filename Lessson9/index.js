c385cc8535d0f774728d0a87a45017438b4d7c40
Mamunarrashid39 <mamunarrashid.cse@gmail.com>
2023-02-07 16:36:44 +0600

    parameter pass in Router

--- Lessson9/index.js
+++ Lessson9/index.js
@@ -2,9 +2,11 @@ const express = require ("express");
 const app = express ();
 const PORT = 3001;
 
-app.get("/", (req , res) => {
- const {id,name} = req.query;
-    res.send(`<h1>Student id is:${name}, id is :${id}</h1>`);
+app.get("/userId/:id/userAge/:age", (req , res) => {
+    const id =req.params.id;
+    const age =req.params.age;
+
+    res.send(`<h1>Student id is : ${id}, age is : ${age}</h1>`);
 });
 
 app.listen(PORT, () => {
