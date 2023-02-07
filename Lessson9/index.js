975000e26fa1055b1bb7fa9f5c5e869ebd1478f4
Mamunarrashid39 <mamunarrashid.cse@gmail.com>
2023-02-07 16:57:58 +0600

    request with header

--- Lessson9/index.js
+++ Lessson9/index.js
@@ -1,15 +1,14 @@
-const express = require ("express");
-const app = express ();
+const express = require("express");
+const app = express();
 const PORT = 3001;
 
-app.get("/userId/:id/userAge/:age", (req , res) => {
-    const id =req.params.id;
-    const age =req.params.age;
+app.get("/", (req, res) => {
+  const id = req.header("id");
+  const name = req.header("name");
 
-    res.send(`<h1>Student id is : ${id}, age is : ${age}</h1>`);
+  res.send(`<h1>Student id is : ${id}, name is : ${name}</h1>`);
 });
 
 app.listen(PORT, () => {
-    console.log (`server is running at http://localhost:${PORT}`);
-
+  console.log(`server is running at http://localhost:${PORT}`);
 });
