e4aea6c824e26aa5ae4566aefa75bd5e4c58c1a4
Mamunarrashid39 <mamunarrashid.cse@gmail.com>
2023-02-24 02:52:02 +0600

    send and recive from data

--- Lessson9/index.js
+++ Lessson9/index.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const app = express();
 const PORT = 3001;
+const bodyParser = require("body-parser");
 
-app.get("/", (req, res) => {
-  const id = req.header("id");
-  const name = req.header("name");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-  res.send(`<h1>Student id is : ${id}, name is : ${name}</h1>`);
+// app.post("/user", (req,res) =>{
+//       const name = req.body.name;
+//       const age = req.body.age;
+//       res.send(`welcome ${name}.your are ${age}.`);
+// });
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+app.post("/register", (req, res) => {
+  const fullName = req.body.fullName;
+  const age = req.body.age;
+  res.send(`<h2> Your name is ${fullName} and age is ${age} </h2>`);
+});
+
+app.post("/register", (req, res) => {
+  res.sendFile();
 });
 
 app.listen(PORT, () => {
